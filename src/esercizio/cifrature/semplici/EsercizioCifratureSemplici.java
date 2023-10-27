f608590d4ebc23f5259a60b8996458ca1f18bc23
Tommaso Melani <melani.tommaso2@studenti-ittfedifermi.edu.it>
2023-10-27 10:50:14 +0200

    Commit Push per Merge

--- src/esercizio/cifrature/semplici/EsercizioCifratureSemplici.java
+++ src/esercizio/cifrature/semplici/EsercizioCifratureSemplici.java
@@ -16,7 +16,7 @@ public class EsercizioCifratureSemplici {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        new MenuPrincipale("Test");
+        new MenuPrincipale("Secret Menu");
     }
     
 }
