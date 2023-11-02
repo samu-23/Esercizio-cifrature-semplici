30298108b89826d9a30ff78437f97ed505ff9b4b
User <maladikshrunbank@gmail.com>
2023-11-02 16:36:33 +0100

    Grafica e Connessione - COMMIT INIZIALE DI EVENTUALE RECUPERO DATI

--- src/esercizio/cifrature/semplici/GUIs/MenuReceiver.java
+++ src/esercizio/cifrature/semplici/GUIs/MenuReceiver.java
@@ -7,6 +7,7 @@ package esercizio.cifrature.semplici.GUIs;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.net.*;
 
 /**
  *
@@ -16,8 +17,14 @@ public class MenuReceiver extends JFrame implements ActionListener {
     
     Container comp = this.getContentPane();
     JPanel northPanel = new JPanel();
+    JPanel centralPanel = new JPanel();
     
     JLabel titoloLabel = new JLabel("Secret Receiver");
+    
+    
+    JLabel portLabel = new JLabel("Port: ");
+    JTextField portTextField = new JTextField();
+    JButton openSocketButton = new JButton("Open connections");
     
     public MenuReceiver(String title, MenuPrincipale menu) {
         
@@ -38,17 +45,50 @@ public class MenuReceiver extends JFrame implements ActionListener {
         titoloLabel.setLocation(WIDTH/2, 0);
         titoloLabel.setFont(new Font("Monospaced", Font.BOLD, 30));
         
+        portTextField.setPreferredSize(new Dimension(100, 20));
+        
         northPanel.add(titoloLabel, BorderLayout.NORTH);
         northPanel.setBorder(BorderFactory.createLineBorder(Color.decode("#D1D1D1"), 5, true));
         northPanel.setBackground(Color.decode("#D1D1D1"));
         
+        openSocketButton.addActionListener(this);
+        
+        centralPanel.add(portLabel);
+        centralPanel.add(portTextField);
+        centralPanel.add(Box.createRigidArea(new Dimension(20,0)));
+        centralPanel.add(openSocketButton);
+        
         comp.add(northPanel, BorderLayout.NORTH);
+        comp.add(Box.createRigidArea(new Dimension(0,20)));
+        comp.add(centralPanel, BorderLayout.CENTER);
         
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        if (e.getSource() == openSocketButton) {
+            
+            try {
+                
+                int port = Integer.parseInt(portTextField.getText());
+                
+                DatagramSocket socketReceiver = new DatagramSocket(port);
+                
+                JOptionPane.showMessageDialog(null, "In attesa di connessioni...");
+                
+                if (socketReceiver.isConnected()) {
+                    
+                }
+                
+                
+            } catch (SocketException ex) {
+                JOptionPane.showMessageDialog(null, "Socket failed to connect: " + ex.getLocalizedMessage());
+            } catch (NumberFormatException nex) {
+                JOptionPane.showMessageDialog(null, "Controlla che la porta sia solo composta da numeri!");
+                portTextField.setText("");
+            }
+            
+        }
     }
     
 }
